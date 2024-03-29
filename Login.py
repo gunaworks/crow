ee12d32bb61e02e940c27c27e450e0a0be5b2667
Charu Chandra Joshi <charu.ccj@gmail.com>
2024-03-29 16:42:17 +0530

    Add github oauth2

--- Login.py
+++ Login.py
@@ -19,43 +19,46 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
+def main():
+    name, authentication_status, username = authenticator.login('main')
+    if authentication_status:
+        try:
+            authenticator.logout('Logout') 
+        except Exception as err:
+            st.error(f'Unexpected exception {err}')
+            raise Exception(err)  # but not this, let's crash the app
+        st.write(f'Welcome *{name}*')
+        st.title('Some content')
+    elif authentication_status is False:
+        st.error('Username/password is incorrect')
+    elif authentication_status is None:
+        st.warning('Please enter your username and password')
 
-name, authentication_status, username = authenticator.login('main')
-if authentication_status:
-    try:
-        authenticator.logout('Logout') 
-    except Exception as err:
-        st.error(f'Unexpected exception {err}')
-        raise Exception(err)  # but not this, let's crash the app
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status is False:
-    st.error('Username/password is incorrect')
-elif authentication_status is None:
-    st.warning('Please enter your username and password')
+    if authentication_status and st.button("Change Password"):
+        try:
+            if authenticator.reset_password(username, 'main'):
+                st.success('Password modified successfully')
+        except Exception as e:
+            st.error(e)
+    # if authentication_status and st.button("Update Details"):
+    #     try:
+    #         if authenticator.update_user_details(username, 'main'):
+    #             st.success('Entries updated successfully')
+    #     except Exception as e:
+    #         st.error(e)
+    # if st.button("Forgot Username"):
+    #     try:
+    #         username_forgot_username, email_forgot_username = authenticator.forgot_username('main')
+    #         if username_forgot_username:
+    #             st.success('Username sent securely')
+    #             # Username to be transferred to the user securely
+    #         else:
+    #             st.error('Email not found')
+    #     except Exception as e:
+    #         st.error(e)
 
-if authentication_status and st.button("Change Password"):
-    try:
-        if authenticator.reset_password(username, 'main'):
-            st.success('Password modified successfully')
-    except Exception as e:
-        st.error(e)
-# if authentication_status and st.button("Update Details"):
-#     try:
-#         if authenticator.update_user_details(username, 'main'):
-#             st.success('Entries updated successfully')
-#     except Exception as e:
-#         st.error(e)
-# if st.button("Forgot Username"):
-#     try:
-#         username_forgot_username, email_forgot_username = authenticator.forgot_username('main')
-#         if username_forgot_username:
-#             st.success('Username sent securely')
-#             # Username to be transferred to the user securely
-#         else:
-#             st.error('Email not found')
-#     except Exception as e:
-#         st.error(e)
-
-with open('config.yaml', 'w') as file:
-    yaml.dump(config, file, default_flow_style=False)
+    with open('config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
+    
+if __name__ == "__main__":
+    main()
