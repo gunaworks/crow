679062aacb1e67369b21a451f29f09c2278203d6
venkateshdukuru <venkateshdukuru2266@gmail.com>
2024-04-04 20:47:38 +0530

    chaged

--- new.py
+++ new.py
@@ -2,8 +2,8 @@ import streamlit as st
 from github import Github
 
 # GitHub credentials
+#this is changed bt me
 github_token = "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"
-#github_token = "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"
 repository_name = "gunaworks/crowci"
 file_name = "config.yaml"
 
