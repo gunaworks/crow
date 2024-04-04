1ba92f543ef9f4a6b9195d072c439888dd5798cd
venkateshdukuru <venkateshdukuru2266@gmail.com>
2024-04-04 20:35:30 +0530

    merge https://github.com/gunaworks/crowci

--- new.py
+++ new.py
@@ -2,11 +2,8 @@ import streamlit as st
 from github import Github
 
 # GitHub credentials
-#<<<<<<< HEAD
-#github_token = "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"
-#=======
 github_token = "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"
-#>>>>>>> 
+#github_token = "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"
 repository_name = "gunaworks/crowci"
 file_name = "config.yaml"
 
