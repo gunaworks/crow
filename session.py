e1b855b703a602373f0a7600e0da8cb641c9a258
venkateshdukuru <venkateshdukuru2266@gmail.com>
2024-04-12 17:29:16 +0530

    session persist

--- session.py
+++ session.py
@@ -1,45 +1,53 @@
 import streamlit as st
-# from oauth import OAuthHandler
 from ui import ui
-import os
 from oauth import OAuthHandler
 import extra_streamlit_components as stx
-import datetime
-# Import other necessary modules or functions
-oauth_handler = OAuthHandler() 
-@st.cache_resource(experimental_allow_widgets=True)
-def get_manager():
-    return stx.CookieManager()
 
-cookie_manager = get_manager()
-# Initialize OAuth handler
+# Initialize OAuth handler and CookieManager
+oauth_handler = OAuthHandler()
+cookie_manager = stx.CookieManager()
+
+# Function to set logout
 def set_logout():
     st.session_state.logout = True
     cookie_manager.delete("token")
+
 # Main function
 def main():
+    # Retrieve token from OAuth handler
     token = oauth_handler.get_token()
+
+    # Retrieve token from cookie manager
     token_val = cookie_manager.get("token")
+
+    # Check if 'logout' is not in session state, if not initialize it
     if 'logout' not in st.session_state:
         st.session_state.logout = False
+
+    # If token exists, store it in cookie
     if token:
         cookie_manager.set("token", token)
-    if token and st.session_state.logout == False:
-        # oauth_handler.set_token(token)
-        # Call other functions or components here
+
+    # If token exists and logout is False, authenticate user and display UI
+    if token and not st.session_state.logout:
         user_info = oauth_handler.get_user_info(token)
         access_token = token['access_token']
         if user_info:
             ui(access_token)
             st.button("Logout", on_click=set_logout)
-    elif token_val and st.session_state.logout == False:
+
+    # If token exists in cookie and logout is False, display UI using token from cookie
+    elif token_val and not st.session_state.logout:
         try:
             access_token = token_val['access_token']
             ui(access_token)
             st.button("Logout", on_click=set_logout)
         except Exception as e:
             oauth_handler.authorize()
+
+    # If no token exists, initiate OAuth authorization flow
     else:
         oauth_handler.authorize()
-if __name__ == "_main_":
+
+if __name__ == "__main__":
     main()
