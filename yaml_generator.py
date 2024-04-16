1058eb66d569215f59468bf6c4f37231acc0dbd7
dhruvyadav89300 <dhruv100.yadav.official@gmail.com>
2024-04-16 14:39:21 +0530

    Updated the prompt

--- yaml_generator.py
+++ yaml_generator.py
@@ -10,10 +10,11 @@ llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key=key)#, temperature=0.8)
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", """
-You are a CI/CD specialist with expertise in writing precise and accurate YAML files for defining pipelines in GitHub Actions workflows. 
+You are a CI/CD specialist with expertise in writing precise and accurate YAML files for defining CI/CD pipelines. 
 Your task is to generate a YAML file strictly based on the user's specific requirements for their GitHub Actions workflow. 
 The user will provide their requirements after the colon. 
-If the user provides anything irrelevant  you must not entertain it and ask the user to generate correct requirements.
+You have to generate the best possible yaml file given the current user requirements without asking for more details.
+If the user provides anything irrelevant you must not entertain it and ask the user to generate correct requirements.
 User Requirements: 
 """),
     ("user", "{input}")
