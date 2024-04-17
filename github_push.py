0a0e8a917344f0e9bec81a4232ef8beeea80541d
Charu Chandra Joshi <charu.ccj@gmail.com>
2024-04-17 21:50:56 +0530

    correct code using pylint

--- github_push.py
+++ github_push.py
@@ -1,48 +1,56 @@
+"""Module used to push the generated yaml content to directory .github/workflows"""
+import re
 import streamlit as st
 from urllib.parse import urlparse
 from github import Github
 from github.Repository import Repository
 import github
-import re
 
-##############################################################################################################
+############################################################################
 def does_object_exists_in_branch(repo: Repository, branch: str, object_path: str) -> bool:
+    """checks whether directory already exists or not"""
     try:
         repo.get_contents(object_path, branch)
         return True
     except github.UnknownObjectException:
         return False
 
-##############################################################################################################
+#############################################################################
 def link_validator(respository_link):
-    return re.search("https://github.com/[\w-]+/[\w-]+$", respository_link)
-    
-##############################################################################################################
+    """Link Validator"""
+    return re.search("https://github.com/[\\w-]+/[\\w-]+$", respository_link)
+
+#############################################################################
 def parse_repository(repository_link):
+    """Parsing the repository link to get the repository name"""
     parsed_url = urlparse(repository_link)
     path_components = parsed_url.path.split('/')
     repository_name = "/".join(path_components[1:3])
     return repository_name
 
-##############################################################################################################
+#############################################################################
 def create_config_file(repository, file_name, yaml_content):
+    """uses create_file"""
     repository.create_file(file_name, "Created YAML file", yaml_content)
     st.success("YAML file created successfully!")
 
-##############################################################################################################
+#############################################################################
 def update_config_file(repository, file_name, yaml_content):
+    """uses update_file"""
     contents = repository.get_contents(file_name)
     repository.update_file(file_name, "Updated YAML file", yaml_content, contents.sha)
     st.success("YAML file updated successfully!")
 
-##############################################################################################################
+#############################################################################
 def get_repository(github_token, repository_name):
+    """get_repository method"""
     g = Github(github_token)
     repository = g.get_repo(repository_name)
     return repository
 
-##############################################################################################################
+##############################################################################
 def push_yaml_to_github(access_token, yaml_content, repository_link):
+    """Checks for validity of link and repository object and then pushes the yaml"""
     github_token = access_token
     file_name = ".github/workflow/config.yaml"
     if link_validator(repository_link):
