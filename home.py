d202a5280b0dcccb4cfe0e5742a157a85ce65008
venkateshdukuru <venkateshdukuru2266@gmail.com>
2024-04-18 19:16:58 +0530

    commit changes

--- home.py
+++ home.py
@@ -1,49 +1,53 @@
-from yaml_generator import generate
+"""This is the home page module"""
 import streamlit as st
-from github_push import push
+from streamlit_extras.stylable_container import stylable_container
+from yaml_generator import generate
+from github_push import push_to_github
 
-# initial_prompt = """ You are a CI specialist. You are only allowed to respond by giving relevant YAML
-#     Pipeline code. You cannot respond with anything else. Only generate most basic YAML pipeline code according to user requirements. Add information according to user and not on your own. Add details as the user provides. The user will give you input after :
-# User Requirements: """
-initial_prompt = "Hi"
-def ui(access_token):
+def home_page(access_token):
+    """Displays the ui after oauth."""
     st.set_page_config(layout="wide")
-
-    left_padding, col1, mid_gap, col2, right_padding = st.columns([1, 1.6 ,.01, 2.5, .5], gap="large")
-    
+    [left_padding, input_column, mid_gap,
+     yaml_output_column,right_padding] = st.columns([.8, 1.8 ,.01, 2.5, .3], gap="large")
     if 'yaml_content' not in st.session_state:
         st.session_state.yaml_content = ""
-
-    with col1:
-        col11, col12 = st.columns([.8,2])
-        with col12:
-            st.title("Input")
+#################################################################################################
+    with input_column:
+        st.markdown("<h1 style='text-align: center; color: #6699CC;'>Input</h1>",
+                    unsafe_allow_html=True)
         with st.form(key="Input", clear_on_submit=False, border=True):
-            user_input = st.text_area(label="Input",height=250 ,key="InputBox", placeholder="Ask CrowCI...", label_visibility="hidden")
-            form_front_gap, form_submit_col =  st.columns([3,1])
-            with form_submit_col:
+            user_input = st.text_area(label="Input",height=250 ,key="InputBox",
+                                      placeholder="Ask CrowCI...", label_visibility="hidden")
+            form_left_padding, form_submit_column =  st.columns([3,1])
+            with form_submit_column:
                 submit = st.form_submit_button("Submit")
-
-
-    with col2:
-        st.markdown("<h1 style='text-align: center; color: #CFF089;'>YAML</h1>", unsafe_allow_html=True)
-        repository_link = st.text_input(label="Repository Name", key = "RepositoryLink", 
-                                        placeholder = "Link to repository...", 
-                                        label_visibility = "collapsed")
-    
-        with st.form(key="Output", clear_on_submit=False, border=True):
-            st.write("YAML Pipeline will be generated here...")
-    
+#################################################################################################
+    with yaml_output_column:
+        st.markdown("<h1 style='text-align: center; color:#6699CC;'>YAML</h1>",
+                    unsafe_allow_html=True)
+        with st.container(height=300, border=True):
+            st.markdown("""<h6 style='text-align: center;color: white;'>
+                        Pipeline generated below...</h6>""",
+                        unsafe_allow_html=True)
+            st.write("")
             if submit:
-                st.session_state.yaml_content = generate(user_input=user_input)
-                st.write(st.session_state.yaml_content)
-    
-            push_to_github = st.form_submit_button(label="Push to Github")  
+                st.session_state.yaml_content = generate(user_input)
+                with stylable_container("codeblock","""code {white-space:
+                                        pre-wrap !important;}""",):
+                    st.code(st.session_state.yaml_content, language = "yaml")
 
-        if push_to_github:
-            if len(st.session_state.yaml_content) and len(repository_link):
-                push(access_token, yaml_content = st.session_state.yaml_content, repository_link = repository_link)
+        repository_link_column, push_to_github_column = st.columns([3.2,1])
+        with repository_link_column:
+            repository_link = st.text_input(label="Repository Name",
+                                            placeholder = "Link to repository...",
+                                            label_visibility = "collapsed")
+        with push_to_github_column:
+            push_to_github_button = st.button(label="Push to Github")
+        if push_to_github_button:
+            if len(st.session_state.yaml_content) > 0 and len(repository_link) > 0:
+                push_to_github(access_token, yaml_content = st.session_state.yaml_content,
+                                    repository_link = repository_link)
             elif len(st.session_state.yaml_content) == 0:
                 st.warning("YAML file empty! Generate response by clicking on submit.")
             else:
-                st.warning("Invalid Repository!")
+                st.warning("Enter the repository link.")
--- yaml_generator.py
+++ yaml_generator.py
@@ -1,31 +1,24 @@
-# import ollama
-# import streamlit as st
-
-# def generate(initial_prompt, user_input):
-#     prompt = initial_prompt + user_input
-#     stream = ollama.generate(
-#         model = "codellama",
-#         prompt = prompt,
-#         stream = True,
-#         )
-#     for chunk in stream:
-#         yield chunk['response']
-
+"""This module is used to generate the yaml file using gpt-3.5-turbo-instruct"""
 import os
-from langchain_openai import ChatOpenAI
-import streamlit as st
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-# key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key="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")#, temperature=0.8)
+load_dotenv()
+
+key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(model="gpt-3.5-turbo-instruct", api_key=key)
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", """
-You are a CI/CD specialist with expertise in writing precise and accurate YAML files for defining pipelines in GitHub Actions workflows. 
+You are a CI/CD specialist with expertise in writing precise and accurate YAML files for defining CI/CD pipelines. 
 Your task is to generate a YAML file strictly based on the user's specific requirements for their GitHub Actions workflow. 
 The user will provide their requirements after the colon. 
-If the user provides anything irrelevant  you must not entertain it and ask the user to generate correct requirements.
+You have to generate the best possible yaml file given the current user requirements without asking for more details.
+If the user provides anything irrelevant you must not entertain it and ask the user to generate correct requirements.
+    Your entire response should be in YAML code format.
 User Requirements: 
 """),
     ("user", "{input}")
@@ -36,5 +29,6 @@ output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
 def generate(user_input):
+    """generate the model response"""
     response = chain.invoke({"input": user_input})
     return response
